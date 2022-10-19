c21563d6a3330789ba85af0254967174b9a671f0
unknown <igormedeiros.f@gmail.com>
2022-10-19 20:20:15 -0300

    mudando nomes

---
+++ atividade3/src/questao2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class questao2 {
+    public static void main(String[] args) throws Exception {
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("DIGITE O N1");
+        int n1 = read.nextInt();
+        System.out.println(" DIGITE O N2");
+        int n2 = read.nextInt();
+        System.out.println(" DIGITE O N3");
+        int n3 = read.nextInt();
+
+   
+
+        if(n1 > n2){
+            System.out.println(" DIGITE O N3");
+        }
+
+
+
+
+     
+    }
+}
