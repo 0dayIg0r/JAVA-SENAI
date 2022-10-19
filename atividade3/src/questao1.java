2580094925f0c0ab58719d2d0d9351e338db3222
unknown <igormedeiros.f@gmail.com>
2022-10-19 20:41:57 -0300

    q1 corrigida

--- atividade3/src/questao1.java
+++ atividade3/src/questao1.java
@@ -7,8 +7,12 @@ public class questao1 {
         System.out.println("DIGITE O N1");
         int n1 = read.nextInt();
       
-        if(n1 > 0){
-         System.out.println(n1 + "É MAIOR QUE 0");
+        if(n1 < 0){
+         System.out.println(n1 + "É MENOR QUE 0");
+        } else if (n1 == 0){
+         System.out.println("DIGITOU O N 0");
+        } else{
+         System.out.println( n1 +"É MAIOR QUE 0");
         }
    
         
