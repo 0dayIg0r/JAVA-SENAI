dbfed9f042313db0239e6e0563df1384493e8c3a
unknown <igormedeiros.f@gmail.com>
2022-10-19 20:49:29 -0300

    alterando numero da atividade

--- atividade3/src/questao3.java
+++ atividade3/src/questao3.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
-public class questao5 {
+public class questao3 {
     public static void main(String[] args) throws Exception {
         Scanner read = new Scanner(System.in);
 
         System.out.println("DIGITE A IDADE");
         int age = read.nextInt();
       
-        if(age < 18){
-         System.out.println(age + "NAO PODE DIRIGIR");
-        } else if (age >= 18){
-         System.out.println("PODE DIRIGIR");
+        if(age >= 16){
+         System.out.println(age + "PODE VOTAR");
+        } else if (age < 16){
+         System.out.println("NÃO PODE VOTAR");
     
         }
     }
