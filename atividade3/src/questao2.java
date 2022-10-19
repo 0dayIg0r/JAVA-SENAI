dfc11550bb951673fe7b746ef6147c9615103744
unknown <igormedeiros.f@gmail.com>
2022-10-19 20:34:58 -0300

    q2

--- atividade3/src/questao2.java
+++ atividade3/src/questao2.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 
-
 public class questao2 {
     public static void main(String[] args) throws Exception {
         Scanner read = new Scanner(System.in);
@@ -12,15 +11,25 @@ public class questao2 {
         System.out.println(" DIGITE O N3");
         int n3 = read.nextInt();
 
-   
+     if(n1 > n2 && n1 > n3){
+        System.out.println( n1 +" É O MAIOR");
 
-        if(n1 > n2){
-            System.out.println(" DIGITE O N3");
-        }
+     } else if ( n2 > n1 && n2 > n3){
+        System.out.println( n2 +" É O MAIOR");
 
+       
+     } else {
+        System.out.println( n3 +" É O MAIOR");
+     }
 
+     if(n2 > n3){
+        System.out.println( n3 +" É O MENOR");
+    }
 
+    if(n1 < n2){
+        System.out.println( n1 +" É O MENOR");
+    }
+        
 
-     
     }
 }
