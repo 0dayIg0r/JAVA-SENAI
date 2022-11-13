2338d039d4c0040c7f2b9b04811c853c0843d73f
unknown <igormedeiros.f@gmail.com>
2022-11-13 11:36:21 -0300

    commit

---
+++ atividade4/atividade4/src/Q5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class Q5 {
+    public static void main(String[] args) throws Exception {
+        Scanner read = new Scanner(System.in);
+      
+
+        System.out.println("IDADE 1");
+        int y1 = read.nextInt();
+        System.out.println("IDADE 2");
+        int y2 = read.nextInt();
+        System.out.println("IDADE 3");
+        int y3 = read.nextInt();
+        System.out.println("IDADE 4");
+        int y4 = read.nextInt();
+        System.out.println("IDADE 5");
+        int y5 = read.nextInt();
+
+
+    }
+
+}
